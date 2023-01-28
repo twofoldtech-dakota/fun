d9cac65938ef1af6da880d30fdeb8e1015f09556
Dakota Smith <73180360+twofoldtech-dakota@users.noreply.github.com>
2023-01-28 16:18:16 -0600

    updates

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -6,7 +6,9 @@ import { PageWrapper } from "../../components/page-wrapper";
 export default function Home() {
   return (
     <PageWrapper title="Settings" className="overflow-hidden ltmd:pt-4 md:p-6">
-      <Container className="flex w-full rounded-lg border-lighter_black p-6 md:border"></Container>
+      <Container className="flex w-full rounded-lg border-lighter_black p-6 md:border">
+        Accounts
+      </Container>
     </PageWrapper>
   );
 }
--- components/page-wrapper.tsx
+++ components/page-wrapper.tsx
@@ -15,16 +15,16 @@ export const PageWrapper = ({
 }) => (
   <div>
     <Header title={title} />
-    <div
-      // initial={{ opacity: 0, y: 20 }}
-      // animate={{ opacity: 1, y: 0 }}
-      // exit={{ opacity: 0, y: 20 }}
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: 20 }}
       className={classNames(
         "relative top-14 min-h-screenHeightWithoutHeader md:top-0",
         className
       )}
     >
       {children}
-    </div>
+    </motion.div>
   </div>
 );
