53a92b56539d68533cdb9761b1187548fbf39da5
Dakota Smith <dakota@twofold.tech>
2023-01-30 07:41:53 -0600

    updating nav links

--- app/head.tsx
+++ app/head.tsx
@@ -1,9 +1,12 @@
+import Script from "next/script";
+
 export default function Head() {
   return (
     <>
       <title>Grasp</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon" href="/favicon.ico" />
+      <Script src="https://code.iconify.design/iconify-icon/1.0.3/iconify-icon.min.js"></Script>
     </>
   );
 }
---
+++ lib/constants.ts
@@ -0,0 +1,4 @@
+export const mainNavigationLinks = [
+  { href: "/dashboard", label: "Dashboard", icon: "lucide:layout-dashboard" },
+  { href: "/screener", label: "Screener", icon: "lucide:bar-chart-3" },
+];
